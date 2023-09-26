2f114c99a4d2a4c4fbef82b13f43ba73e125efa1
Dmitriy Parfimovich <dparf@mail.ru>
2023-09-26 21:08:13 +0300

    Make the first deploy

---
+++ task_manager/views.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from django.shortcuts import render
+from django.views import View
+
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
+
+
+
+
