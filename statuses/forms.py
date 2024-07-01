75e9fb80a501da2cdcbb0d84ec53068b2ba35ef4
Dmitriy Parfimovich <dparf@mail.ru>
2024-07-01 18:53:08 +0300

    Fix building_18

--- statuses/forms.py
+++ statuses/forms.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import re
 from django import forms
 from statuses.models import Status
+from django.utils.translation import gettext as _
 
 
 class NewStatusForm(forms.ModelForm):
@@ -15,6 +17,13 @@ class NewStatusForm(forms.ModelForm):
         status = self.Meta.model(statusname=self.cleaned_data['statusname'])
         status.save()
         return status
+
+    def clean_statusname(self):
+        if re.search(r"^\s*$", self['statusname'].value()):
+            self.errors['statusname'] = _('Required field.')
+        else:
+            self['statusname'].value().strip()
+        return self['statusname'].value()
 
 
 class StatusDeleteForm(forms.ModelForm):
