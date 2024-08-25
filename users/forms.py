6b8e8866c500ce90cc18e68abdd1dc226c460955
Dmitriy Parfimovich <dparf@mail.ru>
2024-08-25 21:54:23 +0300

    Refactoring_36

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,13 @@
 import re
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth import get_user_model
 from users.models import User
+#from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 
 
-class UserRegForm(forms.ModelForm):
+"""class UserRegForm(forms.ModelForm):
 
     password1 = forms.CharField(max_length=20)
     password2 = forms.CharField(max_length=20)
@@ -14,7 +16,7 @@ class UserRegForm(forms.ModelForm):
         model = get_user_model()
         fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
 
-    """def save(self):
+    def save(self):
         self.clean()
         user = self.Meta.model(
             username=self.cleaned_data['username'],
@@ -23,8 +25,37 @@ class UserRegForm(forms.ModelForm):
         )
         user.set_password(self.cleaned_data['password2'])
         user.save()
-        return user"""
+        return user
 
+    def clean_username(self):
+        if re.search(r'[^\w\-@.+]', self['username'].value()):
+            self.errors['username'] = _('Please enter a correct username.\
+                                        It can only contain letters,\
+                                        numbers and symbols @/./+/-/_.')
+        elif User.objects.filter(username=self['username'].value()).exists():
+            self.errors['username'] = _('A user with the same name already exists.')
+        return self['username'].value()
+
+    def clean_password1(self):
+        if self['password1'].value() != self['password2'].value():
+            self.errors['password1'] = _('Entered passwords do not match')
+        elif len(self['password1'].value()) <= 2:
+            self.errors['password1'] = _('The entered password is too short.\
+                                        It must contain at least 3 characters.')
+        return self['password2'].value()"""
+
+class UserRegForm(UserCreationForm):
+
+    """password1 = forms.CharField(max_length=20)
+    password2 = forms.CharField(max_length=20)"""
+
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
+
+    """def clean(self):
+        print(self.errors, 'llllflglglglglglglglglg')"""
+    
     def clean_username(self):
         if re.search(r'[^\w\-@.+]', self['username'].value()):
             self.errors['username'] = _('Please enter a correct username.\
