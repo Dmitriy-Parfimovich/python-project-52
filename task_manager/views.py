f17368f22e2bcdac34c209431aedd85518fed1cf
Dmitriy Parfimovich <dparf@mail.ru>
2024-10-02 22:01:39 +0300

    Refactoring_65

--- task_manager/views.py
+++ task_manager/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 class IndexView(View):
@@ -12,13 +13,15 @@ class IndexView(View):
         return render(request, 'task_manager/index.html')
 
 
-class LoginUserView(LoginView):
+class LoginUserView(SuccessMessageMixin, LoginView):
 
     template_name = 'users/login.html'
+    success_message = _('You are logged in')
+    success_url = reverse_lazy('home')
 
-    def get_success_url(self):
+    """def get_success_url(self):
         messages.add_message(self.request, messages.SUCCESS, _('You are logged in'))
-        return reverse_lazy('home')
+        return reverse_lazy('home')"""
 
 
 class LoginOutView(LogoutView):
