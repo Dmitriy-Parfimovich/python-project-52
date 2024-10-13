6a96651d483eb3ee199469958853ad37ec16bd4d
Dmitriy Parfimovich <dparf@mail.ru>
2024-10-13 20:15:47 +0300

    Refactoring_100

--- tasks/models.py
+++ tasks/models.py
@@ -6,15 +6,15 @@ from labels.models import Label
 
 # Create your models here.
 class Task(models.Model):
-    name = models.CharField(max_length=50, blank=True, unique=True, verbose_name='Имя')
+    name = models.CharField(max_length=50, blank=False, unique=True, verbose_name='Имя')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               blank=True, null=False, related_name="tasks_as_author",
+                               blank=False, related_name="tasks_as_author",
                                verbose_name='Автор')
     executor = models.ForeignKey(User, on_delete=models.CASCADE,
                                  blank=True, null=True, related_name="tasks_as_executor",
                                  verbose_name='Исполнитель')
-    status = models.ForeignKey(Status, on_delete=models.CASCADE, verbose_name='Статус')
+    status = models.ForeignKey(Status, blank=False, on_delete=models.CASCADE, verbose_name='Статус')
     labels = models.ManyToManyField(Label, blank=True, verbose_name='Метки')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
